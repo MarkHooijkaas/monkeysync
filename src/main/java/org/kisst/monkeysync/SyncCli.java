e0f5eb7ab2c9185007c9c8fe22851f1677403fcb
Mark Hooijkaas <mark@hooijkaas.net>
2019-03-05 08:09:45 +0100

    chenage default location of config file to config

--- src/main/java/org/kisst/monkeysync/SyncCli.java
+++ src/main/java/org/kisst/monkeysync/SyncCli.java
@@ -10,12 +10,14 @@ public class SyncCli {
         for (String arg: args) {
             if (arg.equals("-c")||arg.equals("--config"))
                 configFound=true;
+            if (arg.equals("-q")||arg.equals("--quiet"))
+                Env.verbosity=0; // turn off logging to prevent showing the loading config message
             if (arg.indexOf(",")>=0)
                 break;
         }
         // load default config if not specified otherwise
         if (!configFound)
-            Env.loadProps(Paths.get("monkeysync.props"));
+            Env.loadProps(Paths.get("config/monkeysync.props"));
         Script script = new Script(String.join(" ", args));
         script.run();
     }
